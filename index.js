67a6e991d56043bbf3bd098879c194749f592011
henrygd <hank@henrygd.me>
2020-07-20 12:40:50 -0400

    use calculated overflow value instead of auto

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 let html = document.documentElement
 let scrollbarWidth
+let defaultOverflowStyle
 let hidden
 
 let hide = function () {
 	if (hidden) {
 		return
 	}
+	// store existing overflow style
+	defaultOverflowStyle = defaultOverflowStyle || getComputedStyle(html).overflow
+	// calculate scrollbar width if any
 	scrollbarWidth = window.innerWidth - html.clientWidth
+	// hide overflow
 	html.style.overflow = 'hidden'
+	// add padding to compensate for scrollbar and prevent shifting
 	scrollbarWidth && (html.style.paddingRight = `${scrollbarWidth}px`)
 	hidden = true
 }
 
 let show = function () {
-	html.style.overflow = 'auto'
-	scrollbarWidth && (html.style.paddingRight = '0')
+	html.style.overflow = defaultOverflowStyle
+	scrollbarWidth && (html.style.paddingRight = 0)
 	hidden = false
 }
 
